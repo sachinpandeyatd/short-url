8983447e647d1425e75d14f70b8824c30dcb8b01
sachinpandeyatd <33sachinf@gmail.com>
2022-02-06 19:25:30 +0530

    form and table are looking great

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>URL Shortener</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+</head>
+<body>
+    <div class="wrapper">
+        <form action="">
+            <input type="text" name="url" placeholder="Enter URL" required>
+            <i class="url-icon uil uil-link"></i>
+            <button type="submit">Shorten</button>
+        </form>
+        <div class="count">
+            <span>Total Links: <span>10</span> & Total Clicks: <span>140</span></span>
+            <a href="#">Clear All</a>
+        </div>
+        <div class="urls-area">
+            <div class="title">
+                <li>Shorten URL</li>
+                <li>Original URL</li>
+                <li>Clicks</li>
+                <li>Action</li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">shorten.com/xyz</a></li>
+                <li>https://www.sachinpandeyatd.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
